22c24d919bcdd1b331d190abc1ef34fa865bef57
HenTaviShomai <2404469372@qq.com>
2026-06-01 21:33:22 +0800

    new update

--- src/main/java/com/accesscontrolsystem/vo/PermissionTreeVO.java
+++ src/main/java/com/accesscontrolsystem/vo/PermissionTreeVO.java
@@ -6,7 +6,7 @@ import java.util.List;
 @Data
 public class PermissionTreeVO {
     private Long id;
-    private String label;      // 显示名称
-    private String code;       // 权限码
+    private String label;
+    private String code;
     private List<PermissionTreeVO> children;
 }
